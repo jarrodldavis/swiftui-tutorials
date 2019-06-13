b60f8cf872ee5155f9ddc74412432d0a5003ac17
Jarrod Davis <developer@jarrodldavis.com>
2019-06-13 13:07:02 -0600

    Animating Views and Transitions: Section 4
    Compose Animations for Complex Effects

--- AnimatingViewsAndTransitions/StartingPoint/Landmarks/Landmarks/Supporting Views/GraphCapsule.swift
+++ AnimatingViewsAndTransitions/StartingPoint/Landmarks/Landmarks/Supporting Views/GraphCapsule.swift
@@ -20,12 +20,19 @@ struct GraphCapsule: View {
     var offsetRatio: Length {
         Length((range.lowerBound - overallRange.lowerBound) / magnitude(of: overallRange))
     }
-    
+
+    var animation: Animation {
+        Animation.spring(initialVelocity: 5)
+            .speed(2)
+            .delay(0.03 * Double(index))
+    }
+
     var body: some View {
         Capsule()
             .fill(Color.white)
             .frame(height: height * heightRatio, alignment: .bottom)
             .offset(x: 0, y: height * -offsetRatio)
+            .animation(animation)
     }
 }
 
